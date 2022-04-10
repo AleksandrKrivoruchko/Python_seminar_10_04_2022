411a84a63a7bb35ffc2affd03fd7228e506ba6c9
Vladimir Nikolaev <isak25@mail.ru>
2022-04-10 10:32:34 +0200

    End

--- user_interface.py
+++ user_interface.py
@@ -1,5 +1,5 @@
 from read_contact import read_contact
-
+from save_contacts import save_to_csv
 
 def enter_data():
     print('Выберите необходимую операцию:', '1 - ввод данных в формате "Фамилия, Имя, телефон, описание',
@@ -13,9 +13,19 @@ def enter_data():
         phone_number = input('Введите номер телефона: ')
         description = input('Введите описание: ')
         new_list = [soname, name, phone_number, description]
+        save_to_csv(new_list)
         return new_list
     elif operation_select == 2:
         print('Выберите формат отображения данных:', '1 - построчно', '2 - в одну строку', sep='\n')
         read_contact()
+        enter_data()
+    elif operation_select == 3:
+        print('Сделаем потом!!!')
+    elif operation_select == 4:
+        print('До новых встреч!!!')
+        exit(0)
+    else:
+        print('Неверный ввод!!! Повторите!!!')
+        enter_data()
 
-# print(enter_data())
+print(enter_data())
