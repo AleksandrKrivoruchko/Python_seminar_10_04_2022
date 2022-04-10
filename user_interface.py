da94f95e92d56f61a43838326cb3dd0ef5087624
Vladimir Nikolaev <isak25@mail.ru>
2022-04-10 10:38:12 +0200

    Gotovo

--- user_interface.py
+++ user_interface.py
@@ -2,7 +2,7 @@ from read_contact import read_contact
 from save_contacts import save_to_csv
 
 def enter_data():
-    print('Выберите необходимую операцию:', '1 - ввод данных в формате "Фамилия, Имя, телефон, описание',
+    print('Выберите необходимую операцию:', '1 - ввод данных в формате "Фамилия, Имя, телефон, описание"',
           '2 - вывод данных в требуемом формате', '3 - экспорт данных в файл', '4 - завершение работы с модулем',
           sep='\n')
 
@@ -14,10 +14,13 @@ def enter_data():
         description = input('Введите описание: ')
         new_list = [soname, name, phone_number, description]
         save_to_csv(new_list)
+        enter_data()
         return new_list
     elif operation_select == 2:
         print('Выберите формат отображения данных:', '1 - построчно', '2 - в одну строку', sep='\n')
-        read_contact()
+        num = int(input())
+        if num == 1 or num == 2:
+            read_contact(num)
         enter_data()
     elif operation_select == 3:
         print('Сделаем потом!!!')
